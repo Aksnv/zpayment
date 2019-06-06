f08c1b0ad08a3b2cf03296e7f9ca24b855fe3907
Aksnv <aksenov.mailbox@gmail.com>
2019-06-06 16:22:40 +0300

    Доработал карусель для планшетов для любого количества отзывов

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -5,6 +5,7 @@
 var container = document.querySelector(".container");
 var controlsButtonReviewsForward = document.querySelector(".controls__button_reviews_forward");
 var controlsButtonReviewsBack = document.querySelector(".controls__button_reviews_back");
+var reviewsList = document.querySelector(".reviews__list");
 var reviewsItems = document.querySelectorAll(".reviews__item");
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -13,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }*/
 });
 
-controlsButtonReviewsForward.addEventListener("click", function() {
+/*controlsButtonReviewsForward.addEventListener("click", function() {
   if (container.clientWidth !== 320) {
     if (reviewsItems.length > 3) {
       controlsButtonReviewsBack.removeAttribute("disabled");
@@ -22,14 +23,16 @@ controlsButtonReviewsForward.addEventListener("click", function() {
       if (reviewsItems[i].classList.contains("reviews__item_visible")) {
         if (i == reviewsItems.length - 4) {
           reviewsItems[i].classList.remove("reviews__item_visible");
+          reviewsItems[i].classList.remove("reviews__item_current");
           reviewsItems[i + 3].classList.add("reviews__item_visible");
+          reviewsItems[i + 1].classList.add("reviews__item_current");
           controlsButtonReviewsForward.setAttribute("disabled", "disabled");
-          console.log(i);
           break;
         } else {
           reviewsItems[i].classList.remove("reviews__item_visible");
+          reviewsItems[i].classList.remove("reviews__item_current");
           reviewsItems[i + 3].classList.add("reviews__item_visible");
-          console.log(i);
+          reviewsItems[i + 1].classList.add("reviews__item_current");
           break;
         }
       }
@@ -44,17 +47,43 @@ controlsButtonReviewsBack.addEventListener("click", function() {
       if (reviewsItems[j].classList.contains("reviews__item_visible")) {
         if (j == reviewsItems.length - 3) {
           reviewsItems[j].classList.remove("reviews__item_visible");
+          reviewsItems[j - 2].classList.remove("reviews__item_current");
           reviewsItems[j - 3].classList.add("reviews__item_visible");
+          reviewsItems[j - 3].classList.add("reviews__item_current");
           controlsButtonReviewsBack.setAttribute("disabled", "disabled");
-          console.log(j);
           break;
         } else {
           reviewsItems[j].classList.remove("reviews__item_visible");
+          reviewsItems[j - 2].classList.remove("reviews__item_current");
           reviewsItems[j - 3].classList.add("reviews__item_visible");
-          console.log(j);
+          reviewsItems[j - 3].classList.add("reviews__item_current");
           break;
         }
       }
     }
+  }
+});*/
+
+controlsButtonReviewsForward.addEventListener("click", function() {
+  for (var i = 0; i <= reviewsItems.length - 1; i++) {
+    reviewsItems[i].classList.remove("reviews__item_visible");
+    reviewsItems[(i + 3) % reviewsItems.length].classList.add("reviews__item_visible");
+    var reviewsItemNext = reviewsItems[i].cloneNode(true);
+    reviewsItems[i].remove();
+    reviewsList.appendChild(reviewsItemNext);
+    reviewsItems = document.querySelectorAll(".reviews__item");
+    break;
+  }
+});
+
+controlsButtonReviewsBack.addEventListener("click", function() {
+  for (var i = 0; i <= reviewsItems.length - 1; i++) {
+    reviewsItems[i + 2].classList.remove("reviews__item_visible");
+    var reviewsItemPrevious = reviewsItems[reviewsItems.length - 1].cloneNode(true);
+    reviewsItems[reviewsItems.length - 1].remove();
+    reviewsList.insertBefore(reviewsItemPrevious, reviewsList.firstChild);
+    reviewsList.firstChild.classList.add("reviews__item_visible");
+    reviewsItems = document.querySelectorAll(".reviews__item");
+    break;
   }
 });
