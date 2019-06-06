399c832464c67245ec48c092236c0bd9be84ee10
Aksnv <aksenov.mailbox@gmail.com>
2019-06-06 13:07:37 +0300

    Реализовал переключение отзывов для планшетов

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -20,19 +20,41 @@ controlsButtonReviewsForward.addEventListener("click", function() {
     }
     for (var i = 0; i <= reviewsItems.length - 1; i++) {
       if (reviewsItems[i].classList.contains("reviews__item_visible")) {
-        if (i == useCasesItems.length - 4) {
-          controlsButtonReviewsForward.setAttribute("disabled", "disabled");
-          /*break;*/
-        } /*else {*/
+        if (i == reviewsItems.length - 4) {
           reviewsItems[i].classList.remove("reviews__item_visible");
           reviewsItems[i + 3].classList.add("reviews__item_visible");
+          controlsButtonReviewsForward.setAttribute("disabled", "disabled");
+          console.log(i);
           break;
-        /*}*/
+        } else {
+          reviewsItems[i].classList.remove("reviews__item_visible");
+          reviewsItems[i + 3].classList.add("reviews__item_visible");
+          console.log(i);
+          break;
+        }
       }
     }
   }
 });
 
 controlsButtonReviewsBack.addEventListener("click", function() {
-
+  if (container.clientWidth !== 320) {
+    controlsButtonReviewsForward.removeAttribute("disabled");
+    for (var j = reviewsItems.length - 1; j >= 0; j--) {
+      if (reviewsItems[j].classList.contains("reviews__item_visible")) {
+        if (j == reviewsItems.length - 3) {
+          reviewsItems[j].classList.remove("reviews__item_visible");
+          reviewsItems[j - 3].classList.add("reviews__item_visible");
+          controlsButtonReviewsBack.setAttribute("disabled", "disabled");
+          console.log(j);
+          break;
+        } else {
+          reviewsItems[j].classList.remove("reviews__item_visible");
+          reviewsItems[j - 3].classList.add("reviews__item_visible");
+          console.log(j);
+          break;
+        }
+      }
+    }
+  }
 });
